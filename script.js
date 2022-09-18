1617868ec62f96bc6f4d53117cbeda49b7f9929c
unknown <thuranszkyzso@gmail.com>
2022-09-18 17:33:16 +0200

    add select race pic

--- script.js
+++ script.js
@@ -3,58 +3,91 @@ var writeHere = document.getElementById('write-here')
 
 input.addEventListener('input', updateValue)
 
-function updateValue(b) {
-
-    writeHere.textContent = b.target.value
+function updateValue(event) {
+    writeHere.textContent = event.target.value
 }
 
-
 var optionsMaleOrFemale = document.getElementsByName('gender');
-var insertIconOfGenderHere = document.getElementById('selected-gender');
 var selectMaleOrFemale = document.getElementById('gender');
-
-
 
 
 
 selectMaleOrFemale.addEventListener('input', putGenderIcon); 
 
 
-
 function putGenderIcon(event){
-    var female = document.getElementById("female");
-    var male = document.getElementById("male");
-    console.log("event.target.value")
 
     var selectedGenderDiv = document.getElementById('selected-gender');
 
     for( opt of optionsMaleOrFemale) 
     if (event.target.value === "female") {
 
-     
             selectedGenderDiv.innerHTML = `
-        
         <span class="material-symbols-outlined">
         female
-        </span>
-        
+        </span> 
         `
-
         } 
         
         if(event.target.value==="male"){
 
- 
             selectedGenderDiv.innerHTML = `
-        
         <span class="material-symbols-outlined">
         male
         </span>
-        
         `;
-
         }
-
 }
 
+var selectBox = document.querySelector('#race');
 
+var insertSelectedRacePicHereDiv= document.querySelector('#selected-race')
+var imageList = document.getElementsByTagName('img')
+
+// console.log(imageList)
+var imageListArray= Array.from(imageList)
+
+console.log(imageListArray)
+
+var testingImagesClass= document.getElementsByClassName('.images')
+console.log(testingImagesClass)
+
+selectBox.addEventListener('input',makePicVisibleOnCharSheet)
+
+
+
+
+
+
+
+
+var testPara= document.getElementById("test")
+testPara.style.color ="red"
+
+
+
+
+
+
+
+
+
+
+
+
+function makePicVisibleOnCharSheet(event){
+
+    console.log(event.target.value)
+    for (img of imageListArray)
+    if(event.target.value === img.id){
+  
+        img.style.visibility ="visible"
+        console.log("bejött ebbe az elágazásba")
+
+    }else{
+
+        img.style.visibility ="hidden"
+    }
+   
+
+}
